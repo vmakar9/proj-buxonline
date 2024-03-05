4ecb3234173181ce393b3a79c7ed1df2faf38a0c
vmakar9 <zmerin212@gmail.com>
2024-03-05 16:51:24 +0200

    Start project, eslint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
+import { configs } from "./configs/configs";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(5100, async () => {
-  console.log(`Server is running on http://localhost:5100`);
+app.listen(configs.PORT, async () => {
+  console.log(`Server is running on ${configs.PORT} PORT`);
 });
